d7e00889030407b37880a91276e114559610962b
Marco <empleomarcotapia@gmail.com>
2024-04-03 06:53:25 -0700

    Initial Commit

---
+++ Imagenes/ImagenesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagenesApp.swift
+//  Imagenes
+//
+//  Created by Marco on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
