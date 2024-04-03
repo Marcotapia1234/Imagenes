ac4e2af72a7e4183f5050022428d6ed87f45e637
Marco <empleomarcotapia@gmail.com>
2024-04-03 10:58:01 -0700

    Trabajar con imágenes

--- Imagenes/ContentView.swift
+++ Imagenes/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State private var colorChange = false
+    @State private var sizeChange = false
+    
+       var body: some View {
+        Image(systemName: "heart.fill")
+               .font(.system(size: 200))
+                .foregroundColor(colorChange ? .yellow : .red)
+                .scaleEffect(sizeChange ? 1.5 : 1)
+                //.animation(.default)
+                .onTapGesture{
+                    self.colorChange.toggle()
+                    
+                }
+                .onLongPressGesture{
+                    self.sizeChange.toggle()
+                     
+                }
     }
 }
 
